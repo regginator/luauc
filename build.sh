735e9cd0a8adaf5fd8b526b4406b692a784d89e1
regginator <reggie@latte.to>
2023-08-05 12:48:49 -0500

    -O3

--- build.sh
+++ build.sh
@@ -8,5 +8,5 @@ cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --target Luau.VM --config Release
 
 cd ../..
-g++ -std=c++17 -O2 -Wall -Wextra -o luauc luauc.cpp -Lluau/build -lLuau.VM
+g++ -std=c++17 -O3 -Wall -Wextra -o luauc luauc.cpp -Lluau/build -lLuau.VM
 strip -s luauc
