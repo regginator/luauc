9ae0381465cfb2257d7dfc268397ae778336625e
reggie <reggie@latte.to>
2024-01-13 14:53:45 -0600

    Use `c++` instead of `g++` bin in build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ mkdir -p luau/build
 cmake -S luau -B luau/build -DCMAKE_BUILD_TYPE=Release
 cmake --build luau/build --config Release --target Luau.VM
 
-g++ -std=c++17 -O2 -Wall -Wextra -o luauc luauc.cpp -Lluau/build -lLuau.VM
+c++ -std=c++17 -O2 -Wall -Wextra -o luauc luauc.cpp -Lluau/build -lLuau.VM
 strip -s luauc
